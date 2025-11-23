bacfa4e6cad0094a9a8f11e93ba11179958cdced
Raunav Mendiratta <raunav.mendiratta@icloud.com>
2025-11-22 19:15:18 -0800

    Update training.jl

--- src/training.jl
+++ src/training.jl
@@ -1,58 +1,108 @@
-energy_penalty(p, t) = ((sum(p) - sum(t)) / (sum(t) + EPS_F32))^2
+using Lux, Zygote, NNlib
+using ..ECalUDE: EPS_F32, READOUT_THRESH, renorm_to_true
+using ..data: depth_profile, radial_profile, MAX_R, R2_PLANE, X_COORDS, Y_COORDS, S_COORDS
+using ..utils: robust_update
 
-#TRAINING FORWARD & LOSS
+# -----------------------
+# Hyperparameters (v38 relax)
+# -----------------------
+const W_VOX_A    = Float32(10.0)
+const W_SPARSE_A = Float32(0.1)
+const W_VOX_B    = Float32(10.0)
+const W_RAD_B    = Float32(10.0)
+const W_DEPTH_B  = Float32(20.0)
+const RES_RANGE  = Float32(1000.0)
+
+# -----------------------
+# Forward Passes
+# -----------------------
+
 function stageA_forward_raw(m, ps, st, x, tmpl)
-    y, _ = Lux.apply(m, x, ps, st)
-    E_inc_vec = x[5:5, :]
+    y,_ = Lux.apply(m, x, ps, st)
+    E_inc_vec = x[5:5,:]
     y_pos = softplus.(y) .* E_inc_vec
     reshape(y_pos, size(tmpl))
 end
 
-# Modified to accept hyperparameters
-function global_loss_A(p, stA, idxs, events, Xs, modelA, W_HIT, W_SPARSE)
-    l = 0f0
+function stageB_forward(modelB, ps, st, E_A, E_inc)
+    H,W,L = size(E_A)
+
+    geo = cat(
+        repeat(X_COORDS,1,1,L),
+        repeat(Y_COORDS,1,1,L),
+        repeat(sqrt.(R2_PLANE)./MAX_R,1,1,L),
+        reshape(repeat(S_COORDS, inner=H*W),H,W,L),
+        dims=4
+    )
+
+    inp = cat(E_A, log1p.(E_A.+EPS_F32), geo,
+              fill(E_inc,H,W,L), dims=4)
+
+    x = reshape(inp, H,W,L,7,1)
+    y,_ = Lux.apply(modelB, x, ps, st)
+    M = dropdims(y, dims=(4,5))
+
+    ΔE = (E_inc / Float32(H*W*L)) * RES_RANGE .* tanh.(M)
+    return E_A .+ ΔE
+end
+
+# -----------------------
+# Log Profile Loss
+# -----------------------
+function log_profile_loss(pred, true)
+    log_p = log1p.(abs.(pred))
+    log_t = log1p.(abs.(true))
+    return mean(abs.(log_p .- log_t))
+end
+
+# -----------------------
+# Loss Stage A
+# -----------------------
+function global_loss_A(p, stA, idxs, events, Xs, modelA)
+    L = 0f0
     for i in idxs
         pred = stageA_forward_raw(modelA, p, stA, Xs[i], events[i])
-        z_p = log1p.(pred.+EPS_F32); z_t = log1p.(events[i].+EPS_F32)
-        w = map(e -> e > 0f0 ? W_HIT : 1f0, events[i])
-        L_vox = sum(w.*(z_p.-z_t).^2)/sum(w)
+        z_p = log1p.(pred.+EPS_F32)
+        z_t = log1p.(events[i].+EPS_F32)
+
+        # Log-Cosh voxel loss
+        diff = z_p .- z_t
+        L_vox = mean(log.(cosh.(W_VOX_A .* diff)))
+
         L_sparse = mean(abs.(pred))
-        d_p=depth_profile(pred); d_t=depth_profile(events[i])
-        L_depth = mean((d_p.-d_t).^2)/(mean(d_t.^2)+EPS_F32)
-        r_p=radial_profile(pred); r_t=radial_profile(events[i])
-        L_rad = mean((r_p.-r_t).^2)
-        l += L_vox + W_SPARSE*L_sparse + 50f0*L_depth + 50f0*L_rad + 500f0*energy_penalty(pred, events[i])
+
+        dp, dt = depth_profile(pred), depth_profile(events[i])
+        L_depth = log_profile_loss(dp, dt)
+
+        rp, rt = radial_profile(pred), radial_profile(events[i])
+        L_rad = log_profile_loss(rp, rt)
+
+        L += L_vox + W_SPARSE_A*L_sparse + W_DEPTH_B*L_depth + W_RAD_B*L_rad
     end
-    return l/length(idxs)
+    return L/length(idxs)
 end
 
-# Modified to accept hyperparameters
-function stageB_forward(m, ps, st, E_A, E_inc, RES_RANGE)
-    geo = cat(repeat(X_COORDS,1,1,size(E_A,3)), repeat(Y_COORDS,1,1,size(E_A,3)), 
-              repeat(sqrt.(R2_PLANE)./MAX_R,1,1,size(E_A,3)), reshape(repeat(S_COORDS,inner=H_GLOBAL*W_GLOBAL),H_GLOBAL,W_GLOBAL,size(E_A,3)), dims=4)
-    val_scaled = E_A .* 1.0f0 
-    inp = cat(val_scaled, log1p.(E_A.+EPS_F32), geo, fill(E_inc,H_GLOBAL,W_GLOBAL,L_GLOBAL), dims=4)
-    x = reshape(inp, H_GLOBAL, W_GLOBAL, L_GLOBAL, 7, 1)
-    y, _ = Lux.apply(m, x, ps, st)
-    M = dropdims(y, dims=(4,5))
-    scale = (E_inc / Float32(N_VOX_GLOBAL)) * RES_RANGE
-    ΔE = scale .* tanh.(M)
-    E_A .+ ΔE
-end
-
-# Modified to accept hyperparameters
-function global_loss_B(p, stB, idxs, events, EAs, Eincs, modelB, W_VOX_B, W_RAD_B, RES_RANGE)
-    l = 0f0
+# -----------------------
+# Loss Stage B
+# -----------------------
+function global_loss_B(p, stB, idxs, events, EAs, Eincs, modelB)
+    L = 0f0
     for i in idxs
-        raw = stageB_forward(modelB, p, stB, EAs[i], Eincs[i], RES_RANGE)
-        final = renorm_to_true(max.(raw, 0f0), events[i])
-        z_b = log1p.(final.+EPS_F32); z_t = log1p.(events[i].+EPS_F32)
-        L_vox = mean(abs.(z_b .- z_t))
-        d_p=depth_profile(final); d_t=depth_profile(events[i])
-        L_depth = mean((d_p.-d_t).^2)/(mean(d_t.^2)+EPS_F32)
-        r_p=radial_profile(final); r_t=radial_profile(events[i])
-        L_rad = mean((r_p.-r_t).^2)
-        l += W_VOX_B*L_vox + 50f0*L_depth + W_RAD_B*L_rad
+        raw = stageB_forward(modelB, p, stB, EAs[i], Eincs[i])
+        final = renorm_to_true(max.(raw,0f0), events[i])
+
+        z_p = log1p.(final .+ EPS_F32)
+        z_t = log1p.(events[i] .+ EPS_F32)
+
+        L_vox = mean(log.(cosh.(W_VOX_B .* (z_p .- z_t))))
+
+        dp, dt = depth_profile(final), depth_profile(events[i])
+        L_depth = log_profile_loss(dp, dt)
+
+        rp, rt = radial_profile(final), radial_profile(events[i])
+        L_rad = log_profile_loss(rp, rt)
+
+        L += L_vox + W_DEPTH_B*L_depth + W_RAD_B*L_rad
     end
-    return l/length(idxs)
+    return L/length(idxs)
 end
