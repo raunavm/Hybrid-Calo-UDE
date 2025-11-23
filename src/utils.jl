1bf5a22817e837fcc6f8020210bf6d070eeeef29
Raunav Mendiratta <raunav.mendiratta@icloud.com>
2025-11-22 19:14:35 -0800

    Update utils.jl

--- src/utils.jl
+++ src/utils.jl
@@ -1,33 +1,57 @@
-#UTILS
-inverse_softplus(x) = log(exp(max(x, 1f-6)) - 1f0)
-renorm_to_true(p, t) = (sum(p)<=0 || sum(t)<=0) ? p : p .* (sum(t)/sum(p))
+# -----------------------
+# GLOBAL CONSTANTS
+# -----------------------
+const EPS_F32 = Float32(1e-6)
+const READOUT_THRESH = Float32(1e-4)
 
-#GRADIENT CLIPPING
+# -----------------------
+# Utility math
+# -----------------------
+
+inverse_softplus(x) = log(exp(max(x, 1f-6)) - 1f0)
+
+function renorm_to_true(p, t)
+    sum_p = sum(p)
+    sum_t = sum(t)
+    if sum_t <= 0; return p; end
+    if sum_p <= 1e-6
+        return p .+ (sum_t / length(p))
+    end
+    return p .* (sum_t / sum_p)
+end
+
+# -----------------------
+# Gradient Norm + Clipping
+# -----------------------
+
 function grad_norm(g)
     nm = 0f0
-    if g === nothing; return 0f0; end
-    if g isa AbstractArray; return sum(abs2, g); end 
-    if g isa NamedTuple
-        for k in keys(g); nm += grad_norm(g[k]); end
+    if g === nothing
+        return 0f0
+    elseif g isa AbstractArray
+        return sum(abs2, g)
+    elseif g isa NamedTuple
+        for k in keys(g)
+            nm += grad_norm(g[k])
+        end
     end
     return nm
 end
+
 function robust_update(p, g, lr; max_norm=1.0f0)
-    g_sq_sum = grad_norm(g)
-    g_norm = sqrt(g_sq_sum)
-    scale = 1.0f0
-    if g_norm > max_norm; scale = max_norm / (g_norm + Float32(1e-6)); end
-    eff_lr = lr * scale
-    return recursive_apply_update(p, g, eff_lr), g_norm, (scale < 1.0f0)
+    g_sq = grad_norm(g)
+    g_norm = sqrt(g_sq)
+    scale = g_norm > max_norm ? max_norm / (g_norm + 1e-6f0) : 1f0
+    return recursive_apply_update(p, g, lr*scale), g_norm, (scale < 1f0)
 end
-function recursive_apply_update(p::AbstractArray, g::AbstractArray, lr::Float32)
-    return p .- lr .* g
+
+recursive_apply_update(p::AbstractArray, g::AbstractArray, lr) = p .- lr*g
+
+function recursive_apply_update(p::NamedTuple, g::NamedTuple, lr)
+    NamedTuple{keys(p)}(Tuple(
+        haskey(g,k) && g[k] !== nothing ? recursive_apply_update(p[k], g[k], lr) : p[k]
+        for k in keys(p)
+    ))
 end
-function recursive_apply_update(p::NamedTuple, g::NamedTuple, lr::Float32)
-    new_vals = []
-    for k in keys(p)
-        if haskey(g, k) && g[k] !== nothing; push!(new_vals, recursive_apply_update(p[k], g[k], lr)); else; push!(new_vals, p[k]); end
-    end
-    return NamedTuple{keys(p)}(Tuple(new_vals))
-end
+
 recursive_apply_update(p, g, lr) = p
